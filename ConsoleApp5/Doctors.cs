0d49e798d8db148d0c9ebd1958538794b74f14f3
aesrubab <rubabhuseynova013@gmail.com>
2024-06-07 19:30:46 +0400

    Add project files.

---
+++ ConsoleApp5/Doctors.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3;
+public class Doctor
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Experience { get; set; }
+    public Dictionary<string, bool> AcceptionTime { get; set; }
+    public Doctor(string name, string surname, int experince)
+    {
+        Name = name;
+        Surname = surname;
+        Experience = experince;
+        AcceptionTime = new Dictionary<string, bool>
+        {
+            {"9:00-11:00",false},
+            {"12:00-14:00",false},
+            {"15:00-17:00",false }
+
+
+        };
+    }
+    public void ShowAcceptionTime()
+    {
+        foreach (var time in AcceptionTime)
+        {
+            string fullorfree = time.Value ? "Rezerv olunub" : "Rezerv olunmayib";
+            Console.WriteLine($"{time.Key}-{fullorfree}");
+        }
+    }
+}
+
+public class Pediatr : Doctor
+{
+    public Pediatr(string name, string surname, int experince) : base(name, surname, experince)
+    {
+    }
+}
+
+public class Travmatolog : Doctor
+{
+    public Travmatolog(string name, string surname, int experince) : base(name, surname, experince)
+    {
+    }
+}
+
+public class Stamatolog : Doctor
+{
+    public Stamatolog(string name, string surname, int experince) : base(name, surname, experince)
+    {
+    }
+}
+public class Department
+{
+    public string DName { get; set; }
+    public List<Doctor> Doctors { get; set; }
+
+    public Department(string dname)
+    {
+        DName = dname;
+        Doctors = new List<Doctor>();
+    }
+}
+
+public class Acception
+{
+    public string UserName { get; set; }
+    public string UserSurname { get; set; }
+    public string Mail { get; set; }
+    public string Phone { get; set; }
+    public Doctor ChoisenDoctor { get; set; }
+    public string ChoisenTime { get; set; }
+
+    public Acception(string userName, string userSurname, string mail, string phone, Doctor choisenDoctor, string choisenTime)
+    {
+        UserName = userName;
+        UserSurname = userSurname;
+        Mail = mail;
+        Phone = phone;
+        ChoisenDoctor = choisenDoctor;
+        ChoisenTime = choisenTime;
+
+    }
+
+    public void showAccept()
+    {
+        Console.WriteLine($"{UserName} {UserSurname} siz saat {ChoisenTime} da {ChoisenDoctor.Name}{ChoisenDoctor.Surname} qebuluna yazildiz");
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,87 @@
+﻿using ConsoleApp3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Department> departments = new List<Department>();
+
+        Department pediatre = new Department("Pediatr sobesi");
+        pediatre.Doctors.Add(new Pediatr("Aygun", "Bayramov", 10));
+        pediatre.Doctors.Add(new Pediatr("Munevver", "-", 10));
+
+        Department travmatologiya = new Department("Travmatologiya sobesi");
+        travmatologiya.Doctors.Add(new Travmatolog("Rubab", "Huseynova", 10));
+        travmatologiya.Doctors.Add(new Travmatolog("Revan", "-", 10));
+
+        Department stamatologiya = new Department("Stamatologiya sobesi");
+        stamatologiya.Doctors.Add(new Stamatolog("Sabina", "Shakiyeva", 10));
+        stamatologiya.Doctors.Add(new Stamatolog("Nizami", " -", 10));
+
+        departments.Add(pediatre);
+        departments.Add(travmatologiya);
+        departments.Add(stamatologiya);
+
+
+
+        while (true)
+        {
+            Console.Write("Adinizi daxil edin: ");
+            string userName = Console.ReadLine();
+            Console.Write("Soyadinizi daxil edin: ");
+            string userSurname = Console.ReadLine();
+            Console.Write("Emailinizi daxil edin: ");
+            string email = Console.ReadLine();
+            Console.Write("Telefonunuzu daxil edin: ");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("Sobeni secin:");
+            int departmentIndex = 0;
+            int departmentCount = departments.Count;
+            while (departmentIndex < departmentCount)
+            {
+                Console.WriteLine($"{departmentIndex + 1}. {departments[departmentIndex].DName}");
+                departmentIndex++;
+            }
+
+            departmentIndex = int.Parse(Console.ReadLine()) - 1;
+            Department selectedDepartment = departments[departmentIndex];
+
+            Console.WriteLine("Hekimi secin:");
+            int doctorIndex = 0;
+            int doctorCount = selectedDepartment.Doctors.Count;
+            while (doctorIndex < doctorCount)
+            {
+                Console.WriteLine($"{doctorIndex + 1}. {selectedDepartment.Doctors[doctorIndex].Name} {selectedDepartment.Doctors[doctorIndex].Surname}, Experience: {selectedDepartment.Doctors[doctorIndex].Experience} years");
+                doctorIndex++;
+            }
+
+            doctorIndex = int.Parse(Console.ReadLine()) - 1;
+            Doctor selectedDoctor = selectedDepartment.Doctors[doctorIndex];
+
+            string selectedTime = "";
+            while (true)
+            {
+                Console.WriteLine("Rezerv elemek istediyiniz saati secin:");
+                selectedDoctor.ShowAcceptionTime();
+                selectedTime = Console.ReadLine();
+
+                if (selectedDoctor.AcceptionTime.ContainsKey(selectedTime) && !selectedDoctor.AcceptionTime[selectedTime])
+                {
+                    selectedDoctor.AcceptionTime[selectedTime] = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hemen vaxt artiq rezerv olunub, zehmet olmasa basqa bir vaxt secin.");
+                }
+            }
+
+            Acception acception = new Acception(userName, userSurname, email, phone, selectedDoctor, selectedTime);
+            acception.showAccept();
+
+        }
+
+    }
+}
+
